87bfa0134d4794afd623f3c3ac29e827075a9fc4
Vladimir <vladimir@sobaka.com>
2023-12-06 15:41:12 +0300

    grpc routes loader

---
+++ src/Routes/GrpcDispatcherFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FrockDev\ToolsForHyperf\Routes;
+
+use FrockDev\ToolsForHyperf\Annotations\Grpc;
+use Hyperf\Di\Annotation\AnnotationCollector;
+use Hyperf\HttpServer\Router\DispatcherFactory;
+use Hyperf\HttpServer\Router\Router;
+
+class GrpcDispatcherFactory extends DispatcherFactory
+{
+    public function initConfigRoute()
+    {
+        Router::init($this);
+        $grpcControllers = AnnotationCollector::getClassesByAnnotation(Grpc::class);
+
+        Router::addServer('grpc', function () use ($grpcControllers) {
+            foreach ($grpcControllers as $class=>$controller) {
+                $route = $class::GRPC_ROUTE;
+                Router::post('/'.$route, $class.'@__invoke');
+            }
+        });
+    }
+}
