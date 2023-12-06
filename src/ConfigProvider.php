93f92fc2577d3e4fa516f59e4ffbd22fcfce146a
Vladimir <vladimir@sobaka.com>
2023-12-06 15:42:03 +0300

    Config Provider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FrockDev\ToolsForHyperf;
+
+use FrockDev\ToolsForHyperf\Commands\AddGeneratedNamespacesToComposerJson;
+use FrockDev\ToolsForHyperf\Commands\AddToArrayToGrpcObjects;
+use FrockDev\ToolsForHyperf\Commands\CreateEndpointsFromProto;
+use FrockDev\ToolsForHyperf\Commands\PrepareProtoFiles;
+use FrockDev\ToolsForHyperf\Commands\SendEchoToNats;
+use FrockDev\ToolsForHyperf\Listeners\NatsRunListener;
+use FrockDev\ToolsForHyperf\NatsJetstream\NatsJetstreamGrpcDriver;
+use Hyperf\HttpServer\CoreMiddleware;
+use Hyperf\Nats\Driver\NatsDriver;
+
+class ConfigProvider
+{
+    public function __invoke(): array {
+        return [
+            'commands' => [
+                PrepareProtoFiles::class,
+                AddGeneratedNamespacesToComposerJson::class,
+                AddToArrayToGrpcObjects::class,
+                CreateEndpointsFromProto::class,
+                SendEchoToNats::class,
+            ],
+            'dependencies'=>[
+                NatsDriver::class=>NatsJetstreamGrpcDriver::class,
+            ],
+            'listeners'=>[
+                NatsRunListener::class,
+            ]
+        ];
+    }
+}
