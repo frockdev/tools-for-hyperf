3003f8b9e515a60dd789ae17fa3d439c61325547
Vladimir <vladimir@sobaka.com>
2023-12-07 16:55:31 +0300

    fix

--- frock-dev-install-helper.php
+++ frock-dev-install-helper.php
@@ -3,22 +3,22 @@
 $composerJson = json_decode(file_get_contents(getcwd().'/composer.json'), true);
 
 if (getenv('REVERSE')=='0') {
-    if (!isset($composerJson['repositories']['frock_laravel']))
-        $composerJson['repositories']['frock_laravel'] = [
+    if (!isset($composerJson['repositories']['frock_hyperf']))
+        $composerJson['repositories']['frock_hyperf'] = [
             'type' => 'path',
-            'url' => 'frock-laravel',
+            'url' => 'frock-hyperf',
             "options" => [
                 "symlink" => true
             ]
         ];
 
-    $composerJson['require']['frock-dev/tools-for-laravel'] = 'dev-main';
+    $composerJson['require']['frock-dev/tools-for-hyperf'] = 'dev-main';
 } elseif (getenv('REVERSE')=='1') {
-    if (isset($composerJson['repositories']['frock_laravel'])) {
-        unset($composerJson['repositories']['frock_laravel']);
+    if (isset($composerJson['repositories']['frock_hyperf'])) {
+        unset($composerJson['repositories']['frock_hyperf']);
     }
 
-    unset($composerJson['require']['frock-dev/tools-for-laravel']);
+    unset($composerJson['require']['frock-dev/tools-for-hyperf']);
 } else {
     echo 'Please set REVERSE=1 or REVERSE=0';
     exit(1);
